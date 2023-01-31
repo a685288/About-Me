410e558e5cb31628bab71b896e7f36a4ab6c0790
PeiYing <a685288@gmail.com>
2023-01-31 20:47:16 +0800

    feat: header & footer

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,22 @@
   <div id="app">
     <Header />
     <router-view/>
+    <Footer />
   </div>
 </template>
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Footer
   }
-
 }
 </script>
 
 <style lang="sass">
-@import '@/assets/sass/_variables.sass'
-@import '@/assets/sass/reset.scss'
 #app
   font-family: Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/sass/_variables.sass'
+import '@/assets/sass/reset.scss'
 
 createApp(App).use(router).mount('#app')
